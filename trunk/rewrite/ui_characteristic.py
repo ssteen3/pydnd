0cfddee8d802a9c1edc96a09d9a524eac7731051
JSfear <JSfear@ea68c144-a95e-11de-9ed3-3f1bf8abe145>
2010-04-17 02:27:17 +0000

    restructuring the entire system, working towards having client and server databases and eventually to having only sections of database viewable by client. project being worked on again for most part.

--- trunk/rewrite/ui_characteristic.py
+++ trunk/rewrite/ui_characteristic.py
@@ -1,19 +1,23 @@
 from server_characteristics import *
+import pygame
 
 class Game:
     """
     This handles pygame and the interactions between pygame
     and the server Area class
     """
-    def __init__(self, resx=640, resy=480, sqx=32, sqy=24):
+    def __init__(self, host=localhost, port=6000, resx=640, resy=480, sqx=32, sqy=24):
         self.resx = resx
         self.resy = resy
         self.sqx = sqx
         self.sqy = sqy
+        self.anchor = (0,0)
+        self.client = Client(host,port)
         pygame.init()
         self.screen = pygame.display.set_mode((self.resx, self.resy))
 
     def draw_grid(self):
+        """This adds grid to screen based on number of visible squares"""
         color = [0, 0, 0, 0]
         rge = range(0,self.sqy)
         for count in rge:
@@ -22,3 +26,10 @@ class Game:
         rge = range(0,self.sqx)
         for count in rge:
             self.linesw[count] = pygame.draw.line(self.images[self.bg],color,(count*self.resx/self.sqx,0),(count*self.resx/self.sqx,self.resy),1)
+
+    def main_loop(self):
+        while 1:
+            self.update_game()
+            self.update_screen()
+
+    def 
