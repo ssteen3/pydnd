2470f6eed1b69fc08c3ca88d4cbb0f3cc7f82549
JSfear <JSfear@ea68c144-a95e-11de-9ed3-3f1bf8abe145>
2010-04-17 02:27:17 +0000

    restructuring the entire system, working towards having client and server databases and eventually to having only sections of database viewable by client. project being worked on again for most part.

---
+++ rewrite/client_characteristics.py
@@ -0,0 +1,102 @@
+
+import socket
+
+class ClientPlayer:
+    """Player class used by Client."""
+    def __init__(self, n):
+        self.id = n
+
+class Client:
+    """pydnd client
+
+    The client manages a player's game state. It sends messages to and handles
+    messages from the server, interacting with its stored database
+    appropriately. 
+
+    Much of this class was again taken from the dc-rts project.
+    """
+    def __init__(self, hostname, port):
+        """Creates a client that attempts to connect to a server at the given
+        hostname and port."""
+        self.players = list()
+        self.it = 0 # iterator used to track communications frames
+        self.msg = 0 # message buffer
+        self._connect(hostname, port)
+        self.connected = False
+        self.id = 0 # client's corresponding player id
+
+    def tick(self):
+        """Handles all networking and simulation for one communications frame."""
+        while True:
+            if not self._next_msg():
+                break
+            self._lookup_msg()
+        self.it += 1
+
+    def _add_player(self, m):
+        """Adds a new ClientPlayer to self.players"""
+        self.players.append(ClientPlayer(m[1]))
+        print "New Client Player", m[1]
+
+    def _connect(self, host, port):
+        """Requests a connection from the specified server.
+        
+        Sending an empty message with user_id -1 prompts the server to respond
+        with a new player for the connecting client.
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setblocking(0)
+        self.sock.connect((host, port))
+        self._send([-1])
+
+    def _lookup_msg(self):
+        """Calls the function mapped to the key contained in the buffered
+        message.
+        
+        Disconnected clients will only parse messages regarding the player list,
+        and the sync message (used to sync the client's communications message
+        counter with the server)
+        """
+        if self.connected:
+            msg_lu = {"add": self._queue_msg, "attack": self._queue_msg, "move": self._queue_msg, "add_player":
+                self._add_player}
+        else:
+            msg_lu = {"add_player": self._add_player, "is_you": self._set_id,
+                "sync": self._sync}
+        try:
+            msg_lu[self.msg[2]](self.msg)
+        except KeyError:
+            print "Warning: Key", self.msg[1], "in message", \
+                self.msg, "not recognized."
+
+    def _next_msg(self):
+        """Attempts to read next message from socket into self.msg.
+
+        Returns True on success, False on failure.
+        """
+        try:
+            data = self.sock.recv(4096)
+            self.msg = eval(data, {"__builtins__":None}, {}) # eval data safely
+            return True
+        except socket.error, e: # no more data
+            return False
+
+    def _queue_msg(self, m):
+        """Sends messages from the server to the UnitDB queue"""
+        self.database.queue(m)
+
+    def _send(self, m):
+        """Sends message to server"""
+        self.sock.send(repr(m))
+
+    def _set_id(self, m):
+        """Sets client's player id."""
+        self.id = m[1]
+        print "Client is player", m[1]
+
+    def _sync(self, m):
+        """Synchronizes client communications frame counter with server, and
+        mark client as connected."""
+        self.connected = True
+        self.it = m[0]
+        self.database.it = m[0]
--- rewrite/server_characteristics.py
+++ rewrite/server_characteristics.py
@@ -1,36 +1,133 @@
+import socket
+from client_characteristics import ClientPlayer
 
-class ServerArea:
+class ServerPlayer(ClientPlayer):
+    """Player class used by Server."""
+    def __init__(self, n, addr):
+        ClientPlayer.__init__(self, n)
+        self.addr = addr
+
+class Server:
+    """pydnd server
+
+    The server keeps track of players and stores its own database for
+    authoritative purposes. Calls to Server.tick() handle all incoming messages
+    and deal with current matters.
+
+    Most of this server class was taken from the dc-rts project.
     """
-    This class holds information about the field being
-    considered in combat. This version of the class
-    is only created on the server which has all of the
-    information regarding game space.
-    """
-    def __init__(self, x, y, genfile=0):
-        self.x = x
-        self.y = y
-        self.fives = [][]
-        if genfile != 0:
-            self.load_file(genfile);
+    def __init__(self, hostname, port):
+        """Creates a server at the given hostname and port.
+
+        Passing a blank hostname will cause the server to bind to all
+        interfaces.
+        """
+        self.players = list()
+        self.it = 0 # iterator used to track communications frames
+        self.msg = 0 # message buffer
+        self.addr = 0 # address of player whose message is being parsed
+        self._connect(hostname, port)
+
+    def tick(self):
+        """Handles all networking and simulation for one communications frame."""
+        while True:
+            # Exit loop and finish tick if there is no more data.
+            if not self._next_msg():
+                break
+            # Handle message if player is recognized by server.
+            # If player is not recognized, _check_player will add him to
+            # self.players.
+            if self._check_player():
+                self._lookup_msg()
+        self.it += 1
+
+    def combat_tick(self):
+        self.unit_db.combat_tick(self.weapon_db)
+        self.weapon_db.tick()
+
+    def _check_player(self):
+        """Reads the message in self.msg to determine whether or not the player
+        is in the server's players list, self.players. If the player does not
+        exist, the server sends him an ID and adds him to the list.
+        
+        Returns True if player exists, otherwise False.
+        """
+        if self.msg[0] == -1:
+            if self.players:
+                new_id = self.players[-1].id + 1
+            else:
+                new_id = 0
+            self._new_player(new_id)
+            return False
         else:
-            self.load_random();
+            try:
+                self.players[self.msg[0]]
+                return True
+            except IndexError:
+                print "Warning: Player ID", self.msg[0], "in message", \
+                    self.msg, "not recognized."
+                return False
 
-    def load_file(self, genfile):
-       """This will load a specified map area from a file when needed."""
-       pass
+    def _connect(self, hostname, port):
+        """Binds the given UDP port on the interface specified by hostname.
 
-    def load_random();
-        """This will generate a map randomly based on generic map
-        characteristics. for now just loads empty map"""
-        for i=1:self.x:
-            for j=1:self.y:
-               self.fives[i][j] = FiveByFive(0)
+        A blank hostname will create a socket that listens on all interfaces.
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((hostname, port))
+        self.sock.setblocking(0)
 
-class FiveByFive:
-    """
-    This class controlls all of the contents of a five by five area of
-    the field, as well as the type of the area. e.g. wall cliff, elevation
-    """
-    def __init__(self, type=0, contents=[]):
-        self.type = type
-        self.contents = contents
+    def _lookup_msg(self):
+        """Calls the function mapped to the key contained in the buffered
+        message"""
+        msg_lu = {"add": self._queue_msg, "attack": self._queue_msg, "move": self._queue_msg}
+        try:
+            msg_lu[self.msg[1]](self.msg)
+        except KeyError:
+            print "Warning: Key", self.msg[1], "in message", \
+                    self.msg, "not recognized."
+
+    def _new_player(self, id):
+        """Creates a player and notifies clients."""
+        # The order here is important! If the new player is added to
+        # self.players before notifying all clients, then the new player will
+        # get the message twice.
+        self._send_all([id, "add_player"])
+        self.players.append(ServerPlayer(id, self.addr))
+        # tell the new player who the other players are
+        for p in self.players:
+            self._send_to([p.id, "add_player"], id)
+        # tell the new player which one is him
+        self._send_to([id, "is_you"], id)
+        self._send_to([id, "sync"], id)
+        print "New Server Player", id
+
+    def _next_msg(self):
+        """Attempts to read next message from socket into self.msg.
+
+        Returns True on success, False on failure.
+        """
+        try:
+            data, self.addr = self.sock.recvfrom(4096)
+            self.msg = eval(data, {"__builtins__":None}, {}) # eval data safely
+            return True
+        except socket.error, e: # no more data
+            return False
+
+    def _queue_msg(self, m):
+        """Passes off messages to clients and server's own UnitDB instance."""
+        self._send_all(m)
+        self.unit_db.queue(m)
+
+    def _send_all(self, m):
+        """Sends the given message to all players."""
+        m.insert(0, self.it + 2)
+        for p in self.players:
+            self.sock.sendto(repr(m), p.addr)
+
+    def _send_to(self, m, id):
+        """Sends the given message to the specified player."""
+        m.insert(0, self.it + 2)
+        self.sock.sendto(repr(m), self.players[id].addr)
+    
+
